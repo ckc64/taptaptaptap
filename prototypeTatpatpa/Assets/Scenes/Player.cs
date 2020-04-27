9a5fe6885958799f3838060e210734e4bcaf3138
Charles Kurt <32605261+ckc64@users.noreply.github.com>
2020-04-27 21:52:57 +0800

    added

---
+++ prototypeTatpatpa/Assets/Scenes/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+
+{
+
+    [SerializeField] private int jumpForce = 10;
+    public Rigidbody2D rigidbody;
+ 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log(Vector2.up);
+            rigidbody.velocity = Vector2.up * jumpForce;
+        }
+    }
+}
